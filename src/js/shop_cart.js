5ac4d0178ed3fe22ecf06b1febc6b7099a91b472
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-03-31 14:04:02 -0500

    add list shops

---
+++ src/js/shop_cart.js
@@ -0,0 +1,30 @@
+const d = document,
+	$aside = d.querySelector('.aside'),
+	$shoppingNumber = d.querySelector('.shopping-cart b')
+
+function toggleAside({ target }) {
+	if (target.matches('.burger-menu') || target.matches('.aside-back')) {
+		$aside.classList.toggle('hidden')
+	}
+}
+
+function payShop({ target }) {
+	if (target.matches('#pay-shop')) {
+		target.textContent = '...'
+		setTimeout(() => {
+			target.textContent = 'Pagado con éxito'
+			setTimeout(() => {
+				location.pathname = 'index.html'
+			}, 500)
+		}, 300)
+	}
+}
+
+d.addEventListener('click', (e) => {
+	toggleAside(e)
+	payShop(e)
+})
+
+d.addEventListener('DOMContentLoaded', (e) => {
+	$shoppingNumber.textContent = 0
+})
